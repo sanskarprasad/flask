f8ed9f2215de607907520915e47a6cef8cf05ea1
sanskarprasad <psanskarkumar01@gmail.com>
2024-11-03 00:19:33 +0530

    change cred

--- flask/app/config.py
+++ flask/app/config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://username:password@localhost:27017/testdb")
+    MONGO_URI = os.getenv("MONGO_URI")
